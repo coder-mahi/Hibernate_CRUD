aa16f39b9af5753cc962d3d837397546b8bb5ea5
MAHESH SHINDE <120265441+coder-mahi@users.noreply.github.com>
2025-01-09 17:13:36 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,9 @@ public class Main{
 		cfg.configure("hibernate.cfg.xml");
 		SessionFactory factory = cfg.buildSessionFactory();
 		Session session = factory.openSession();
-		CRUD obj =(CRUD) session.get("com.practice.entity.CRUD",1);
+		CRUD obj =new CRUD();
+	//READ
+		obj = (CRUD) session.get("com.practice.entity.CRUD",1);
 		if(obj!=null){
 			System.out.println("ID :"+obj.getId());
 			System.out.println("Name :"+obj.getName());
@@ -19,6 +21,24 @@ public class Main{
 		}else{
 			System.out.println("Record not found..!");			
 		}
+	//INSERT or UPDATE
+		CRUD obj2 = new CRUD();
+		Transaction tx =null;
+		try{
+		tx = session.beginTransaction();
+		//obj2.setId(2);                ---> Id not need to mention because use of generator class in mapping file
+		obj2.setName("Tester");
+		obj2.setCompany("wipro");
+		obj2.setSalary(10000.01);
+		session.saveOrUpdate(obj2);
+		tx.commit();
+		System.out.println("Record inserted Successfully...");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+//DELETE 
+	//working....
+		
 		session.close();
 	}
 }
